79940e06c974554c5ed8465a5972ed4560c3ceb1
Heloísa Paschoal <heloisa.gp2001@gmail.com>
2025-05-19 19:56:29 -0300

    Add files via upload

---
+++ desafio_cypress/login.page copy.js
@@ -0,0 +1,10 @@
+const INPUT_EMAIL = '#email'
+const INPUT_SENHA = '#passwd'
+const BTN_LOGIN = '#SubmitLogin'
+
+Cypress.Commands.add('realizarLogin', () => {
+    cy.get(INPUT_EMAIL).type(Cypress.env('username'), {log: false})
+    cy.get(INPUT_SENHA).type(Cypress.env('password'), {log: false})
+    cy.wait(4000)
+    cy.get(BTN_LOGIN).click()
+})
